e7c4494db8b38f2b1aa15ff6152421d80d8f126e
Virendra9892 <virendrap9892@gmail.com>
2022-11-08 10:52:50 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// import events from "events";
+// import { setTimeout } from "timers";
+// const event = new events.EventEmitter();
+
+// event.on("first",(arg)=>{
+//     setTimeout(()=>{
+//         console.log("First event occured !",arg);
+//     },2000);
+// });
+// event.on("first",(arg)=>{
+//    setTimeout(()=>{
+//     console.log("Second event occured !",arg);
+//    },3000);
+// });
+
+// event.on("first",(arg)=>{
+//     setTimeout(()=>{
+//         console.log("Third event occured !",arg);
+//     },4000)
+// });
+// event.on("first",(arg)=>{
+//     setTimeout(()=>{
+//         console.log("Fourth event occured !",arg);
+//     },5000)
+// });
+// event.emit("first",{Name:"Virendra",Id:1520191629});
+
+// import EventEmitter from "events";
+// const event = new EventEmitter();
+
+// event.on("First",(names,sts)=>{
+//     console.log(`My name is ${names} and status code is ${sts}`);
+// })
+
+
+// event.emit("First","Virendra",200);
